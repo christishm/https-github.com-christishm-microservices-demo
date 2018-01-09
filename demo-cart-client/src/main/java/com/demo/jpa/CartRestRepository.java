5689b11b0dcc8aaf7537f62733cb8dc174178d13
christishm <35049166+christishm@users.noreply.github.com>
2018-01-09 09:50:01 +0530

    Added Java docs

--- demo-cart-client/src/main/java/com/demo/jpa/CartRestRepository.java
+++ demo-cart-client/src/main/java/com/demo/jpa/CartRestRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.demo.model.CartModel;
 
 /**
- Declarative REST template which exposes all the DAO methods in Repistory Layer as RESTful apis.
+ Declarative REST template which exposes all the DAO methods in Repository Layer as RESTful apis.
 */
 @RepositoryRestResource(path="/")
 public interface CartRestRepository extends PagingAndSortingRepository<CartModel, Long>{
