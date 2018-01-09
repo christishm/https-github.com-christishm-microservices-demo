dfc070b6f0f78f9615e17d68e5faacf7d9ef7960
christishm <35049166+christishm@users.noreply.github.com>
2018-01-09 08:14:23 +0530

    Calling out design patterns used

--- demo-gateway-zuul/src/main/java/com/demo/DemoGatewayApplication.java
+++ demo-gateway-zuul/src/main/java/com/demo/DemoGatewayApplication.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+/**
+This class configures Gateway API for all the microservices to communicate and collaborate.
+DesignPattern Used: Facade pattern which provides an unified interface for all microservices providing security,caching, routing capabilities.
+*/
 @SpringBootApplication
 @EnableZuulProxy
 @EnableFeignClients
