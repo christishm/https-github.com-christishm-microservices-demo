998287bfe24fb959c029b833c1abb5e390a3a867
christishm <35049166+christishm@users.noreply.github.com>
2018-01-09 09:14:53 +0530

    Adding Java Docs

--- demo-product-client/src/main/java/com/demo/jpa/ProductRestRepository.java
+++ demo-product-client/src/main/java/com/demo/jpa/ProductRestRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.demo.model.Product;
 
+/**
+Declarative REST template which exposes all the DAO methods in Repistory Layer as RESTful apis.
+*/
 @RepositoryRestResource(path="/")
 public interface ProductRestRepository extends PagingAndSortingRepository<Product, Long>{
 
