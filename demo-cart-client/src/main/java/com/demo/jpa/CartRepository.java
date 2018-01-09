9d9018579c9c4cf7b74ab2f2e2b467307fe77936
christishm <35049166+christishm@users.noreply.github.com>
2018-01-09 09:49:09 +0530

    Added Java docs

--- demo-cart-client/src/main/java/com/demo/jpa/CartRepository.java
+++ demo-cart-client/src/main/java/com/demo/jpa/CartRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.demo.model.CartModel;
 
+/**
+Repositoy Design Pattern is used to provide data persistence and retrieval abstraction for the Domain Model objects.
+Hibernate based implentation for JPA interfaces.Uses auto configured datasource(provided by Spring Boot).
+*/
 
 public interface CartRepository extends CrudRepository<CartModel,Long>{
 	public List<CartModel> findByUserId(String userId);
