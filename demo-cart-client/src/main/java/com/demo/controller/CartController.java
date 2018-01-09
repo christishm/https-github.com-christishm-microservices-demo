51498e2d9f0cc5bc4b5f86c0d98a98b2103453a8
christishm <35049166+christishm@users.noreply.github.com>
2018-01-10 05:08:00 +0530

    Update CartController.java

--- demo-cart-client/src/main/java/com/demo/controller/CartController.java
+++ demo-cart-client/src/main/java/com/demo/controller/CartController.java
@@ -17,7 +17,7 @@ import com.demo.service.CartService;
 
 /**
 Controller class used to map incoming client requests to service layer.
-FrontEndController design pattern using auto configured DispatcherViewServlet(using SpringBoot autoconfiguration).
+FrontEndController design pattern using auto configured DispatcherServlet(using SpringBoot autoconfiguration).
 The auto configured DispatcherViewServlet will have mappings to all service URLs.
 */
 
