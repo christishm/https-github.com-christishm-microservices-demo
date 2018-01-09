5d4a6af81b8de43c33fa7923570c2955a4249628
christishm <35049166+christishm@users.noreply.github.com>
2018-01-09 08:56:07 +0530

    Calling design patterns used

--- demo-cart-client/src/main/java/com/demo/jpa/CartEntryRepository.java
+++ demo-cart-client/src/main/java/com/demo/jpa/CartEntryRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.demo.model.CartEntry;
 
-
+/**
+Design pattern Repositoy Pattern is used to provide data persistence and retrieval abstraction for the Domain Model objects.
+Hibernate based implentation for JPA interfaces.
+*/
 public interface CartEntryRepository extends CrudRepository<CartEntry,Long>{
 	
 	List<CartEntry> findByProductId(String productId);
