71061861dc16f00ca8f903b969a04efad019ab27
christishm <35049166+christishm@users.noreply.github.com>
2018-01-09 08:19:52 +0530

    Adding documentation for Design patterns used

--- demo-gateway-zuul/src/main/java/com/demo/service/CartClientApiService.java
+++ demo-gateway-zuul/src/main/java/com/demo/service/CartClientApiService.java
@@ -8,6 +8,10 @@ public interface CartClientApiService {
 	
 	ResponseEntity<String> addToCart(String productId);
 	
+	/**
+	 Using Circuit breaker design pattern to mitigate cascading failures when service is not available.
+	 'fallbackMethod' attribute specifies method which will be called when client service is not available.
+	*/
 	@HystrixCommand(fallbackMethod="getLoggedInDemoUser")
 	String getLogInIdForUser();
 	
